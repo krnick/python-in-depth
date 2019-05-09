4f087d1983c6bd7df7a6a574f0f4ea0cfe486a96
krnick <sungboss2004@gmail.com>
2019-05-09 15:09:56 +0800

    yield from 完成

--- README.md
+++ README.md
@@ -174,3 +174,68 @@ print(next(result))
 1. 子生成器 (放在代理生成器後面的函數)
 2. 代理生成器 (做雙向溝通)
 3. 調用者 (做 ```send``` 資料和接收 ```yield``` 回來的參數)
+
+在程式碼加入了可辨識的英文，大家可以跟著英文找尋程式執行的路線。
+
+```python
+def download_from_url(url):
+    url = str(url)
+    print("Downloading:"+url)
+
+
+def average_gen():
+    """
+    子生成器
+    """
+    url = "預設值"
+
+    print("A")
+
+    while True:
+        print("B")
+        # 接收 send 過來的值
+        new_url = yield url  
+        print("C")
+        # 輸出 send 過來的值
+        download_from_url(new_url)  
+
+        if new_url is None:
+            break
+
+    # 每一次return，代表這個協程結束
+    return "ok"
+
+
+def proxy_gen():
+    """
+    代理生成器
+
+    只有子生成器要結束 return了，yield from
+    左邊的變數 result 才會被賦值，後面的程式才會被執行
+    """
+    print("D")
+    while True:
+        print("E")
+
+        result = yield from average_gen()
+        print("F")
+        print(result)
+
+
+def main():
+    """
+    調用者
+    """
+    web_download = proxy_gen()
+    next(web_download)                  # 啟動
+    web_download.send("www.gmail.com")  # 下載第一個網頁
+    web_download.send(None)             # 結束協程
+    """
+    這裡若是在web_download.send("text")，由於上一個
+    協程已經結束，這裡會重新啟動一個新的
+    """
+
+
+if __name__ == '__main__':
+    main()
+```
--- threadpool_download.py
+++ threadpool_download.py
@@ -1,47 +1,57 @@
-def gen_range(name):
-    index = 0
-    while index < len(name):
-        # 請分成兩個步驟
-        # 1. yield name[index] =>把一個元素彈出去
-        # 2. 若有東西被 send 進來，會存到 yield 裡面
-
-        jump = yield name[index]
-
-        print("jump 被賦值"+str(jump))
-
-        if jump is None:
-            jump = 1
-        index += jump
+import time
 
 
-test_name = gen_range("Nick")
-# print(next(test_name))
-# print(next(test_name))
-# print(next(test_name))
-# print(next(test_name))
+def download_from_url(url):
+    url = str(url)
+    print("Downloading:"+url)
 
-"""
-Output:
 
-N
-jump 被賦值None
-i
-jump 被賦值None
-c
-jump 被賦值None
-k
+def average_gen():
+    """
+    子生成器
+    """
+    url = "預設值"
 
-"""
-print(next(test_name))
-print(test_name.send(2))  # 現在 index 變成 2
-print(next(test_name))  # 這裡應該輸出 'k'
+    print("A")
 
-"""
-Output:
+    while True:
+        print("B")
+        new_url = yield url  # 接收 send 過來的值
+        print("C")
 
-N
-jump 被賦值2
-c
-jump 被賦值None
-k
-"""
+        download_from_url(new_url)  # 輸出 send 過來的值
+
+        if new_url is None:
+            break
+
+    # 每一次return，代表這個協程結束
+    return "ok"
+
+
+def proxy_gen():
+    """
+    代理生成器
+    """
+    print("D")
+    while True:
+        print("E")
+        # 只有子生成器要結束 return了，yield from左邊的變數 result 才會被賦值，後面的程式才會被執行
+        result = yield from average_gen()
+        print("F")
+        print(result)
+
+
+def main():
+    """
+    調用者
+    """
+    web_download = proxy_gen()
+    next(web_download)                  # 啟動
+    web_download.send("www.gmail.com")  # 下載第一個網頁
+    web_download.send(None)             # 結束協程
+
+    # 這裡若是在web_download.send("text")，由於上一個協程已經結束，這裡會重新啟動一個新的
+
+
+if __name__ == '__main__':
+    main()
